6ec0f7bdaf1138850c3a13937cc8fe36ef5392b9
Vedran Mandić <mandic.vedran@gmail.com>
2019-04-18 22:33:49 +0200

    add test code

--- Program.fs
+++ Program.fs
@@ -1,8 +1,46 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿//these are similar to C# using statements
+open canopy.runner.classic
+open canopy.configuration
+open canopy.classic
 
-open System
+// this was not in the tutoiral, had to point out the chrome driver manually
+let directory = 
+    System.Reflection.Assembly.GetEntryAssembly().Location 
+    |> System.IO.Path.GetDirectoryName
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+chromeDir <- directory
+
+//start an instance of chrome
+start chrome
+
+//this is how you define a test
+"taking canopy for a spin" &&& fun _ ->
+    //this is an F# function body, it's whitespace enforced
+
+    //go to url
+    url "http://lefthandedgoat.github.io/canopy/testpages/"
+
+    //assert that the element with an id of 'welcome' has
+    //the text 'Welcome'
+    "#welcome" == "Welcome"
+
+    //assert that the element with an id of 'firstName' has the value 'John'
+    "#firstName" == "John"
+
+    //change the value of element with
+    //an id of 'firstName' to 'Something Else'
+    "#firstName" << "Something Else"
+
+    //verify another element's value, click a button,
+    //verify the element is updated
+    "#button_clicked" == "button not clicked"
+    click "#button"
+    "#button_clicked" == "button clicked"
+
+//run all tests
+run()
+
+printfn "press [enter] to exit"
+System.Console.ReadLine() |> ignore
+
+quit()
